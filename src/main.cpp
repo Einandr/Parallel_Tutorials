7c77bd47b8a5895ed1e92f058ec9c1147255bea0
Einandr <yacfd@yandex.ru>
2024-12-28 16:39:38 +0300

    added new files froms SRC

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+
+
+int i;
+int arr_length = 3;
+float A[] = {1,2,3};
+float B[] = {4,5,6};
+float *C = (float *)malloc(arr_length * sizeof(float));
+
+
+
+
+int main()
+{
+
+
+
+for (i = 0; i < arr_length; i++)
+    C[i] = A[i] + B[i];
+
+for (i = 0; i < arr_length; i++)
+std::cout << "A: " << A[i] << "B: " << B[i] << "C: " << C[i] << std::endl;
+
+
+}
