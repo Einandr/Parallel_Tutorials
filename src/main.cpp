b320e5735db530ba640655e11fd6a52bc897324f
Einandr <yacfd@yandex.ru>
2024-12-30 17:14:39 +0300

    reconfugured for random array filling

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include <cstdlib>
 #include <iostream>
 #include <chrono>
+#include <stdlib.h>
 
 
 int i;
-int arr_length = 3;
-float A[] = {1,2,3};
-float B[] = {4,5,6};
+int arr_length = 100;
+// float A[] = {1,2,3};
+// float B[] = {4,5,6};
+float *A = (float *)malloc(arr_length * sizeof(float));
+float *B = (float *)malloc(arr_length * sizeof(float));
 float *C = (float *)malloc(arr_length * sizeof(float));
 
-
-float Add[] = {1,2,3};
 
 int main()
 {
 
+for (i = 0; i < arr_length; i++)
+	{
+		int numA = std::rand()%arr_length;
+        int numB = std::rand()%arr_length;
+		A[i]=numA;
+        B[i]=numB;
+	}
 
 
 for (i = 0; i < arr_length; i++)
